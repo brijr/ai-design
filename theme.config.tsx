3b9f177c36fe9d900f75a6cc85957d4a52bf8bf5
Bridger Tower <bridgertower@gmail.com>
2024-11-06 15:28:48 -0700

    wip

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Design for AI</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/brijr/ai-design",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/brijr/ai-design",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Design for AI",
   },
-}
+};
 
-export default config
+export default config;
